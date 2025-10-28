2f62dc8027bc0b4eca3fd8437e9316391fd7fc18
Karol Jurzec <karol.jurzec.programista@gmail.com>
2025-10-28 16:06:03 +0100

    Add side-menu open/close functionality

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function showSideMenu() {
+  document.querySelector("nav").classList.toggle("open");
+  document.querySelector("nav").classList.add("side-menu");
+}
 
+function closeSideMenu() {
+  document.querySelector("nav").classList.toggle("open");
+  document.querySelector("nav").classList.remove("side-menu");
 }
 
 document.querySelector(".menu-icon").addEventListener("click", showSideMenu);
+document.querySelector(".sidebar-close-icon").addEventListener("click", closeSideMenu);
